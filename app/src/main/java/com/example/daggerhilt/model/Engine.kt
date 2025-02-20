d5bb34a6c7f5d96dc10ec1cb7c43e58596f300d4
RehanGith <chrehan1007@gmail.com>
2025-02-20 08:29:28 +0500

    Created model class and injected it in mainActivity

---
+++ app/src/main/java/com/example/daggerhilt/model/Engine.kt
@@ -0,0 +1,3 @@
+package com.example.daggerhilt.model
+
+data class Engine(val name : String)
--- app/src/main/java/com/example/daggerhilt/ui/MainActivity.kt
+++ app/src/main/java/com/example/daggerhilt/ui/MainActivity.kt
@@ -7,16 +7,19 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.daggerhilt.R
 import com.example.daggerhilt.databinding.ActivityMainBinding
+import com.example.daggerhilt.model.Engine
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    @Inject lateinit var engine: Engine
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        binding.text.text = engine.name
     }
 }
